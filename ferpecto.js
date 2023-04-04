b08cfecc3af09b2463a956883ed40aa628f4e8f8
HajerPrzodowy <129874236+HajerPrzodowy@users.noreply.github.com>
2023-04-04 17:16:14 +0200

    Add files via upload

---
+++ ferpecto.js
@@ -0,0 +1,22 @@
+function Zespół (wokalista, gitarzysta1, gitarzysta2, basista, perkusista)
+{
+    PowołajGitarzystę(gitarzysta1);
+    PowołajGitarzystę (gitarzysta2);
+    PowołajWokalistę (wokalista);
+    PowołajBasistę (basista);
+    PowołajPerkusistę (perkusista);
+}
+
+function PowołajGitarzystę (_gitarzysta1, _gitarzysta2)
+{}
+
+function PowołajWokalistę (_wokalista)
+{}
+
+function PowołajBasistę (_basista)
+{}
+
+function PowołajPerkusistę (_perkusista)
+{}
+
+
